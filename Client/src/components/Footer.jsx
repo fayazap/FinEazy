388ffc16a34f64834950bdb312474cafde3d93ff
fayazap <fayazap2@gmail.com>
2022-08-18 17:09:33 +0530

    Footer Change

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -5,11 +5,9 @@ import logo from "../../images/logo1.png";
 const Footer = () => (
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
     <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
-      <div className="flex flex-[0.5] justify-center items-center">
-        <img src={logo} alt="logo" className="w-32" />
-      </div>
-      <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
+      
+      <div className="flex justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
+        <p className="text-white text-base text-center  mx-2 cursor-pointer">Market</p>
         <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
         <p className="text-white text-base text-center mx-2 cursor-pointer">Tutorials</p>
         <p className="text-white text-base text-center mx-2 cursor-pointer">Wallets</p>
@@ -25,6 +23,9 @@ const Footer = () => (
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
       <p className="text-white text-left text-xs">@FinEazy2022</p>
+      <div className="flex flex-[0.5] justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 bg-black" />
+      </div>
       <p className="text-white text-right text-xs">All rights reserved</p>
     </div>
   </div>
